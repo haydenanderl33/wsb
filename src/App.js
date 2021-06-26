a1665b0ac57868ad003805f5ecfe6162f0a72074
haydenanderl33 <hayden.anderl33@gmail.com>
2021-06-26 15:50:48 -0600

    add firebase setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import firebase from 'firebase';
 function App() {
+//  console.log(firebase)
+
   return (
     <div className="App">
       <header className="App-header">
